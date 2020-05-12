902bc60852d41b30d74d9deebda7f1160223c512
Alexander Koval <kovalidis@gmail.com>
2020-05-12 23:43:55 +0300

    Added sqlalchemy, connect to mysql

--- pyramid_tutorial/app.py
+++ pyramid_tutorial/app.py
@@ -1,6 +1,17 @@
-from waitress import serve
 from pyramid.config import Configurator
 from pyramid.response import Response
+
+from sqlalchemy import engine_from_config
+from sqlalchemy.orm import scoped_session
+from sqlalchemy.orm import sessionmaker
+
+from waitress import serve
+
+from zope.sqlalchemy import register as register_session
+
+
+db_session = scoped_session(sessionmaker())
+register_session(db_session)
 
 
 def hello_world(request):
@@ -9,9 +20,13 @@ def hello_world(request):
 
 
 def main(global_config, **settings):
+    from pprint import pprint
+
     with Configurator(settings=settings) as config:
+        engine = engine_from_config(settings, 'sqlalchemy.')
+        pprint(engine.execute('select version()').fetchone())
+        db_session.configure(bind=engine)
         config.add_route('hello', '/')
         config.add_view(hello_world, route_name='hello')
-        import pprint
-        pprint.pprint(config.get_settings())
+        pprint(config.get_settings())
         return config.make_wsgi_app()
