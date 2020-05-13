dc02d054245058d6b60956eec468e3ff64af784d
Alexander Koval <kovalidis@gmail.com>
2020-05-13 18:23:28 +0300

    XSS

--- pyramid_tutorial/app.py
+++ pyramid_tutorial/app.py
@@ -1,3 +1,5 @@
+import logging
+
 from pyramid.config import Configurator
 from pyramid.response import Response
 
@@ -9,16 +11,32 @@ from .models import db_session
 from .models import Product
 
 
+log = logging.getLogger(__name__)
+
+
 def hello_world(request):
     print('Incoming request')
     products = db_session.query(Product).all()
     return Response(f'<body><h1>Hello World!</h1></body>')
 
 
+def products(request):
+    products = (
+        db_session.query(Product)
+        .filter(Product.status == 0)
+        .limit(10)
+        .all()
+    )
+    product_list_html = '\n'.join(f'<p>{p.id}<p>{p.name}' for p in products)
+    return Response(f'<body><h1>Our store products!</h1>{product_list_html}</body>')
+
+
 def main(global_config, **settings):
-    with Configurator(settings=settings) as config:
-        engine = engine_from_config(settings, prefix='sqlalchemy.')
-        db_session.configure(bind=engine)
-        config.add_route('hello', '/')
-        config.add_view(hello_world, route_name='hello')
-        return config.make_wsgi_app()
+    config = Configurator(settings=settings)
+    engine = engine_from_config(settings, prefix='sqlalchemy.')
+    db_session.configure(bind=engine)
+    config.add_route('hello', '/')
+    config.add_view(hello_world, route_name='hello')
+    config.add_route('products', '/products')
+    config.add_view(products, route_name='products')
+    return config.make_wsgi_app()
